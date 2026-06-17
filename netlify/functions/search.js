c35e01812732d671efb9e4a0aaf21cf63dbf5717
CharlesGriffith44 <781charlie@gmail.com>
2026-06-17 16:17:00 +0200

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -15,7 +15,7 @@ exports.handler = async function (event) {
       },
       body: JSON.stringify({
         model: "claude-haiku-4-5-20251001",
-        max_tokens: 1200,
+        max_tokens: 2800,
         messages: body.messages,
       }),
     });
